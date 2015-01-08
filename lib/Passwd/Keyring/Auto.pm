d26c87d84b97790ab936945d8f8a652a879ea063
Marcin Kasperski <Marcin.Kasperski@address.censored.com>
2015-01-08 15:50:23 +0000

    Import of MEKK/Passwd-Keyring-Auto-0.2801.tar.gz from CPAN.
    gitpan-cpan-distribution: Passwd-Keyring-Auto
    gitpan-cpan-version:      0.2801
    gitpan-cpan-path:         MEKK/Passwd-Keyring-Auto-0.2801.tar.gz
    gitpan-cpan-author:       MEKK
    gitpan-cpan-maturity:     released

--- lib/Passwd/Keyring/Auto.pm
+++ lib/Passwd/Keyring/Auto.pm
@@ -4,6 +4,7 @@ use warnings;
 use strict;
 use base 'Exporter';
 our @EXPORT = qw(get_keyring);
+use Carp;
 
 =head1 NAME
 
@@ -11,17 +12,17 @@ Passwd::Keyring::Auto - interface to secure password storage(s)
 
 =head1 VERSION
 
-Version 0.2704
+Version 0.2801
 
 =cut
 
-our $VERSION = '0.2704';
+our $VERSION = '0.2801';
 
 =head1 SYNOPSIS
 
-Passwd::Keyring is about securely preserving passwords and similar
-sensitive data applications use in backends like Gnome Keyring, KDE
-Wallet, OSX/Keychain etc.
+Passwd::Keyring is about securely preserving passwords and other
+sensitive data (for example API keys, OAuth tokens etc) in backends
+like Gnome Keyring, KDE Wallet, OSX/Keychain etc.
 
 While modules like Passwd::Keyring::Gnome handle specific backends,
 Passwd::Keyring::Auto tries to pick the best backend available,
@@ -43,16 +44,17 @@ considering the current desktop environment.
     }
 
 If any secure backend is available, password is preserved
-for successive runs, and users need not be prompted.
+for successive runs, and user need not be prompted.
 
-The choice can be impacted by some environment variables,
-see C<get_keyring> documentation for details.
+The choice can be impacted by some environment variables and/or
+additional parameters, see C<get_keyring> documentation for details.
 
-Finally, one can skip this module and be explicit:
+One can skip this module and be explicit if he or she knows which
+keyring is to be used:
 
     use Passwd::Keyring::Gnome;
     my $keyring = Passwd::Keyring::Gnome->new();
-    # ... and so on
+    # ... from there as above
 
 =head1 EXPORT
 
@@ -64,27 +66,165 @@ get_keyring
 
     my $ring = get_keyring()
 
-    my $ring = get_keyring(app=>'symbolic application name', group=>'symbolic group/folder/.. name');
+    my $ring = get_keyring(app=>'MyApp', group=>'SyncPasswords');
 
-    my $ring = get_keyring(app=>'...', group=>'...', %backend_specific_options);
+    my $ring = get_keyring(app=>'MyApp', group=>'Scrappers',
+                           prefer=>['Gnome', 'PWSafe3'],
+                           forbid=>['KDEWallet']);
 
-Returns the keyring object most appropriate for the current
-system. Passess all options received to this backend. See
-L<Passwd::Keyring::Auto::KeyringAPI> for available operations on
-keyring and their semantic.
+    my $ring = get_keyring(app=>'MyApp', group=>'Scrappers',
+                           force=>['KDEWallet']);
 
-Note: setting environment variable PASSWD_KEYRING_AUTO_DEBUG
-causes the routine to print to stderr details about tried and
-selected keyrings.
+    my $ring = get_keyring(app=>'MyApp', group=>'SyncPasswords',
+                           %backend_specific_options);
 
-The default choice can be influenced by environment variables:
+Returns the keyring object most appropriate for the current system
+(and matching specified criteria) and initiates it.
 
-- C<PASSWD_KEYRING_AUTO_FORBID> - name or space separated names of keyrings
-  which can't be used, for example C<Gnome> or C<Gnome KDEWallet>
-  (note: Memory can't be forbidden, but is always last)
+The function inspects context the application runs in (operating
+system, presence of GUI sessions etc), decides which backends seem suitable
+and in what order of preference, then tries all suitable backends and returns
+first succesfully loaded and initialized.
 
-- C<PASSWD_KEYRING_AUTO_PREFER> - name or space separated names of keyrings
-  to prefer
+All parameters are optional, but it is recommended to set app and
+group:
+
+=over 4
+
+=item app => 'App Name'
+
+Symbolic application name, which - depending on backend - may appear
+in interactive prompts (like dialog box "Application APP-NAME wants
+to access secure data..." popped up by KDE Wallet) and may be
+preserved as comment ("Created by ...") in secure storage (so may be
+seen in GUI password management apps like seahorse).
+
+=item group => 'PasswordFolder'
+
+The name of the passwords folder. Can be visualised as folder or group
+by some GUIs (seahorse, pwsafe3) but it's most important role is to
+let one separate passwords used for different purposes. A few
+apps/scripts will share passwords if they use the same group name, but
+will use different and unrelated passwords if they specify different
+group.
+
+=item force => 'Backend'
+
+Try only given backend and nothing else. Expects short backend name.
+For example C<force=>'Gnome'> means L<Passwd::Keyring::Gnome> is to be
+used and nothing else.
+
+=item prefer=>'Backend'    or    prefer => ['Backend1', 'Backend2', ...]
+
+Try this/those backends first, and in the specified order (and try them
+even if by default they are not considered suitable for OS in use).
+
+For example C<prefer=>['OSXKeychain', 'KDEWallet']> asks module to try
+L<Passwd::Keyring::OSXKeychain> first, then
+L<Passwd::Keyring::KDEWallet>, then other options (if any) in module
+own preference.
+
+=item forbid=>'Backend'     or    forbid => ['Backend1', 'Backend2', ...]
+
+Never use specified backend(s).
+
+For example C<forbid=>['Gnome', 'KDEWallet']> will disable attempts to
+use GUI keyrings even if we run on Linux and have Gnome or KDE session
+active.
+
+=item other parameters
+
+All other parameters are passed as such to actual keyring backend.
+To check whether/which may be used, consult backends documentation.
+In general backends ignore params they do not know.
+
+=back
+
+The function should not in normal circumstances fail (there always is
+L<Passwd::Keyring::Memory> to be used if everything else fails), but
+it may croak if some keyring is enforced or if Memory is forbidden or
+uninstalled.
+
+=head1 KEYRING METHODS
+
+See L<Passwd::Keyring::Auto::KeyringAPI> for operations available on
+keyring objects.
+
+=head1 ENVIRONMENT VARIABLES
+
+The following environment variables can be used to impact the module behaviour:
+
+=over 4
+
+=item C<PASSWD_KEYRING_AUTO_FORCE> 
+
+Use given backend and nothing else. For example, by setting
+C<PASSWD_KEYRING_AUTO_FORCE=KDEWallet> user may enforce use of
+L<Passwd::Keyring::KDEWallet>.
+
+This variable is completely ignored if C<force> parameter was
+specified, and causes runtime error if specified backend is not
+present, not working, or present on the C<forbid> list.
+
+=item C<PASSWD_KEYRING_AUTO_FORBID>
+
+Space separated list of backends to forbid, for example
+C<PASSWD_KEYRING_AUTO_FORBID="Gnome KDEWallet">.
+
+Ignored if C<force> parameter was specified, otherwise works as this
+param.
+
+=item C<PASSWD_KEYRING_AUTO_PREFER> 
+
+Space separated names of backends to prefer.
+
+Ignored if C<prefer> parameter was specified, otherwise works as this
+param.
+
+=back
+
+The following variable provides some additional logging:
+
+=over 4
+
+=item C<PASSWD_KEYRING_AUTO_DEBUG>
+
+Log on stderr details about tried and selected backends (and errors
+faced while they are tried).
+
+=back
+
+=head1 BACKEND SELECTION AND PREFERENCE CRITERIA
+
+By default (no C<force>, C<prefer> or C<forbid> params, no environment
+variables) the following criteria are used (note that those may change
+without warning and are described here just for illustration):
+
+=over 4
+
+=item Linux/Unix
+
+Passwd::Keyring::Gnome and Passwd::Keyring::KDEWallet are tried first
+(if Gnome session is detected, Gnome version is first, under KDE and
+in unclear context KDEWallet takes preference). If both fail, emergency
+Passwd::Keyring::Memory is returned.
+
+=item Mac OS/X
+
+Passwd::Keyring::OSXKeychain is tried, if it does not work, 
+Passwd::Keyring::Memory is returned.
+
+=item Windows
+
+Currently Passwd::Keyring::Memory is always returned (this is to
+change once Windows Vault backend is written).
+
+=back
+
+Note: some backends are not considered unless asked for (for example
+L<Passwd::Keyring::PWSafe3> is not currently considered by default
+algorithm).
+
 
 =cut
 
@@ -93,15 +233,33 @@ sub get_keyring {
 
     my $debug = $ENV{PASSWD_KEYRING_AUTO_DEBUG} ? 1 : 0;
 
-    my @forbidden = split(/\s+/, $ENV{PASSWD_KEYRING_AUTO_FORBID} || '');
-    my @preferred = split(/\s+/, $ENV{PASSWD_KEYRING_AUTO_PREFER} || '');
+    my $force = $options{force} || $ENV{PASSWD_KEYRING_AUTO_FORCE} || '';;
+    my $forbid = $options{forbid} || [ split(/\s+/, $ENV{PASSWD_KEYRING_AUTO_FORBID} || '') ];
+    my $prefer = $options{prefer} || [ split(/\s+/, $ENV{PASSWD_KEYRING_AUTO_PREFER} || '') ];
+    delete $options{forbid}; delete $options{prefer}; delete $options{force};
+
+    unless(ref($forbid)) {
+        $forbid = [$forbid];
+    }
+    unless(ref($prefer)) {
+        $prefer = [$prefer];
+    }
+
+    #################################################################
+    # Fast path for force
+    #################################################################
+
+    if($force) {
+        my $keyring = _try_backend($force, $debug, %options);
+        return $keyring if $keyring;
+        croak "Can not load enforced keyring $force";
+    }
 
     #################################################################
     # Selection and scoring of possible options.
     #################################################################
-    
-    # Note: we prefer to check possibly wrong module than to
-    # miss it.
+
+    # Note: we prefer to check possibly wrong module than to miss some.
 
     my %candidates =(  # name → score, score > 0 means possible
         'Gnome' => 0,
@@ -110,8 +268,8 @@ sub get_keyring {
         'Memory' => 1,
         );
 
-    # Scoring: +100 for preferrable in given env, +10 for sensible,
-    # +1 for possible
+    # Scoring: +1000 for preferred, +100 for session-related, +10 for
+    # sensible, +1 for possible
 
     if($^O eq 'darwin') {
         $candidates{'OSXKeychain'} += 100;
@@ -130,13 +288,13 @@ sub get_keyring {
         $candidates{'KDEWallet'} += 10;
     }
 
-    $candidates{$_} += 1000 foreach (@preferred);
-    delete $candidates{$_} foreach (@forbidden);
+    $candidates{$_} += 1000 foreach (@$prefer);
+    delete $candidates{$_} foreach (@$forbid);
 
     my @attempts = grep { $candidates{$_} > 0 } keys %candidates;
 
     @attempts = sort { ($candidates{$b} <=> $candidates{$a})
-                       || 
+                       ||
                        ($a cmp $b)
                    } @attempts;
 
@@ -145,33 +303,47 @@ sub get_keyring {
           join(", ", map { "$_($candidates{$_})" } @attempts), "\n";
     }
 
-
     foreach my $keyring_name (@attempts) {
-        my $keyring;
-        my $require = "Passwd/Keyring/$keyring_name.pm";
-        my $module = "Passwd::Keyring::$keyring_name";
-        eval {
-            require $require;
-            $keyring = $module->new(%options);
-        };
-        if($debug) {
-            unless($@) {
-                print STDERR "[Passwd::Keyring::Auto] Succesfully initiated $keyring_name, returning it\n";
-            } else {
-                print STDERR "[Passwd::Keyring::Auto] Attempt to use $keyring_name failed, error: $@\n";
-            }
-        }
+        my $keyring = _try_backend($keyring_name, $debug, %options);
         return $keyring if $keyring;
     }
 
-    # Last resort if sth went wrong
-    require Passwd::Keyring::Memory;
-    return Passwd::Keyring::Memory->new(%options);
+    croak "Could not load any keyring backend (attempted: " . join(", ", @attempts) . ")";
+}
+
+# Loads module of given name or returns undef if it does not work
+sub _try_backend {
+    my ($backend_name, $debug, @options) = @_;
+
+    # Sanity check
+    unless($backend_name =~ /^[A-Za-z][A-Za-z0-9_]*$/) {
+        if($debug) {
+            print STDERR "[Passwd::Keyring::Auto] Ignoring illegal backend name: $backend_name\n";
+        }
+        return undef;
+    }
+
+    my $keyring;
+    my $require = "Passwd/Keyring/$backend_name.pm";
+    my $module = "Passwd::Keyring::$backend_name";
+    eval {
+        require $require;
+        $keyring = $module->new(@options);
+    };
+    if($debug) {
+        unless($@) {
+            print STDERR "[Passwd::Keyring::Auto] Succesfully initiated $module, returning it\n";
+        } else {
+            print STDERR "[Passwd::Keyring::Auto] Attempt to use $module failed, error: $@\n";
+        }
+    }
+    return $keyring;
 }
 
 =head1 FURTHER INFORMATION
 
-L<Passwd::Keyring::Auto::KeyringAPI> describes backends API in detail.
+L<Passwd::Keyring::Auto::KeyringAPI> describes methods available on keyring objects
+and provides some additional detail on keyring construction.
 
 =head1 AUTHOR
 
@@ -179,7 +351,7 @@ Marcin Kasperski
 
 =head1 BUGS
 
-Please report any bugs or feature requests to 
+Please report any bugs or feature requests to
 issue tracker at L<https://bitbucket.org/Mekk/perl-keyring-auto>.
 
 =head1 SUPPORT
@@ -198,7 +370,7 @@ L<https://bitbucket.org/Mekk/perl-keyring-auto>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2012 Marcin Kasperski.
+Copyright 2012-2015 Marcin Kasperski.
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
@@ -209,4 +381,4 @@ See http://dev.perl.org/licenses/ for more information.
 
 =cut
 
-1; # End of Keyring
+1; # End of Passwd::Keyring::Auto
--- t/21-finds_proper_keyring_envs.t
+++ t/21-finds_proper_keyring_envs.t
@@ -3,12 +3,24 @@
 use strict;
 use warnings;
 use Test::More;
+use File::Temp;
 
 BEGIN { use_ok("Passwd::Keyring::Auto", qw(get_keyring)) };
 
+# Minimal env grabbing
+my @diagn;
+
+foreach my $variable (qw(DESKTOP_SESSION)) {
+    push @diagn, "$variable=$ENV{$variable}" if $ENV{$variable};
+}
+foreach my $variable (qw(DISPLAY GNOME_DESKTOP_SESSION_ID DBUS_SESSION_BUS_ADDRESS GNOME_KEYRING_CONTROL GNOME_KEYRING_PID)) {
+    push @diagn, "$variable present" if $ENV{$variable};
+}
+diag(join(", ", @diagn));
+
 # Under Gnome good keyring should be picked
 SKIP: {
-    skip "Not a Gnome session", 3 unless ($ENV{DESKTOP_SESSION} || '') =~ /^(gnome.*|ubuntu)$/i;
+    skip "Not a Gnome session", 3 unless $ENV{GNOME_DESKTOP_SESSION_ID} || ($ENV{DESKTOP_SESSION} || '') =~ /^(gnome.*|ubuntu)$/i;
     eval { require Passwd::Keyring::Gnome };
     skip "Passwd::Keyring::Gnome not installed", 3 if $@;
 
@@ -22,7 +34,7 @@ SKIP: {
 
 # ... but not if it is forbidden
 SKIP: {
-    skip "Not a Gnome session", 3 unless ($ENV{DESKTOP_SESSION} || '') =~ /^(gnome.*|ubuntu)$/i;
+    skip "Not a Gnome session", 3 unless $ENV{GNOME_DESKTOP_SESSION_ID} || ($ENV{DESKTOP_SESSION} || '') =~ /^(gnome.*|ubuntu)$/i;
     eval { require Passwd::Keyring::Gnome };
     skip "Passwd::Keyring::Gnome not installed", 3 if $@;
 
@@ -69,6 +81,19 @@ SKIP: {
 }
 
 SKIP: {
+    skip "Not a desktop session", 3 
+      unless ($ENV{DESKTOP_SESSION});
+
+    local $ENV{PASSWD_KEYRING_AUTO_FORBID} = 'KDEWallet Gnome';
+
+    my $ring = get_keyring(app_name=>"Passwd::Keyring::Auto unit tests", group=>"Passwd::Keyring::Auto");
+    ok($ring, "Got some keyring");
+
+    unlike(ref($ring), qr/::KDEWallet/, "We should respect FORBID");
+    unlike(ref($ring), qr/::Gnome/, "We should respect FORBID");
+}
+
+SKIP: {
     skip "Not a Mac", 3 unless $^O eq 'darwin';
     eval { require Passwd::Keyring::OSXKeychain };
     skip "Passwd::Keyring::OSXKeychain not installed", 3 if $@;
@@ -81,6 +106,21 @@ SKIP: {
     isa_ok($ring, "Passwd::Keyring::OSXKeychain", "Under darwin we should get OSXKeychain keyring");
 }
 
+SKIP: {
+    eval { require Passwd::Keyring::PWSafe3; };
+    skip "Passwd::Keyring::PWSafe3 not installed", 3 if $@;
+
+    local $ENV{PASSWD_KEYRING_AUTO_FORCE} = "PWSafe3";
+    my ($fh, $filename) = File::Temp::tempfile();
+    my $ring = get_keyring(app_name=>"Passwd::Keyring::Auto unit tests", group=>"Passwd::Keyring::Auto",
+                           master_password=>"Very Secret Password",
+                           file=>$filename);
+    ok($ring, "Got some keyring");
+
+    ok($ring->is_persistent, "We should get persistent keyring");
+
+    isa_ok($ring, "Passwd::Keyring::PWSafe3", "With PASSWD_KEYRING_AUTO_FORCE=PWSafe3, got " . ref($ring));
+}
 
 done_testing;
 
